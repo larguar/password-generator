e8403acb1f1363348f9bee12fa77c2b63b9bdfd3
siminski <laurensiminski@gmail.com>
2020-06-03 02:02:07 -0400

    alert added when no boxes are checked

--- js/main.js
+++ js/main.js
@@ -27,54 +27,67 @@
     
 }());
 
-// Generate password
+// Generate password functionality
 function generatePassword() { 
 	
+	// Start with blank values for password and character options
 	var password = ''; 
+	var includeElements = '';
 	
+	// Add value options for each check box
 	var includeLowercase = 'abcdefghijklmnopqrstuvwxyz';
 	var includeUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	var includeNumbers = '01234567890123456789';
 	var includeSymbols = '@%+\/!#$?@%+\/!#$?';
 	
-	var includeElements = '';
-	
+	// If lowercase box is checked, add lowercase values towards character options
 	if (document.getElementById('includeLowercase').checked === true) {		
 		var includeElements = includeElements + includeLowercase;		
 	}
 	
+	// If uppercase box is checked, add uppercase values towards character options
 	if (document.getElementById('includeUppercase').checked === true) {		
 		var includeElements = includeElements + includeUppercase;		
 	}
 	
+	// If numbers box is checked, add numbers values towards character options
 	if (document.getElementById('includeNumbers').checked === true) {		
 		var includeElements = includeElements + includeNumbers;		
 	}
 	
+	// If symbols box is checked, add symbols values towards character options
 	if (document.getElementById('includeSymbols').checked === true) {		
 		var includeElements = includeElements + includeSymbols;		
 	}
 	
+	// If none of the boxes are checked, alert the user
 	if (document.getElementById('includeLowercase').checked === false &&
 		document.getElementById('includeUppercase').checked === false && 
 		document.getElementById('includeNumbers').checked === false && 
 		document.getElementById('includeSymbols').checked === false) {
 		
-		console.log('Error code goes here');
+		alert('You must check at least one of the boxes.');
 		
 	}
 	
+	// Pull the number the user selected for password length
 	var passwordLength = document.getElementById('passwordLength');
 	var lengthValue = passwordLength.options[passwordLength.selectedIndex].text;
-
+	
+	// For the number of characters the user selected, generate a random assortment of characters based on the character options the user selected
+	
+	// Sets character count to the number the user selected
 	for (i = 1; i <= lengthValue; i++) { 
 		
+		// Generates a random collection of characters out of the checked values
 		var char = Math.floor(Math.random() * includeElements.length + 1); 
 		password += includeElements.charAt(char);
-	} 
-	
+	} 	
 	return password; 
-} 
+	
+}
+
+// Add result to my ID when function takes place (when button is clicked)
 function readySetGo() { 
 	document.getElementById('randomResult').value = generatePassword();
 } 
