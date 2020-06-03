c65ce40f7b1ad1038eae1ebe5fee20265b7bd4e5
siminski <laurensiminski@gmail.com>
2020-06-03 01:17:06 -0400

    generator functioning with static password length

--- js/main.js
+++ js/main.js
@@ -1,28 +1,77 @@
-// add a huge range of numbers to form select automatically
+// Add a huge range of numbers to form select automatically
 (function() {
 	
-	// get the select by ID
+	// Get the select by ID
 	var select = document.getElementById('passwordLength'),
     
-    // create a document fragment to hold the options while we create them
+    // Create a document fragment to hold the options while we create them
     fragment = document.createDocumentFragment();
     
-    // specify number range for select
+    // Specify number range for select
     for (var i = 8; i <= 128; i++) {
 	    
-	    // creates the <option> element around numbers
+	    // Creates the <option> element around numbers
         var option = document.createElement('option');
         
-        // outputs numbers
+        // Outputs numbers
         option.value = i;
         
-        // set custom text content
+        // Set custom text content
         option.appendChild(document.createTextNode(i));
         
-        // append <option> to the document fragment
+        // Append <option> to the document fragment
         fragment.appendChild(option);
     }
-    // append the document fragment to the DOM (vs. setting inner HTML a bunch of times)
+    // Append the document fragment to the DOM (vs. setting inner HTML a bunch of times)
     select.appendChild(fragment);
     
 }());
+
+// Generate password
+function generatePassword() { 
+	
+	var password = ''; 
+	
+	var includeLowercase = 'abcdefghijklmnopqrstuvwxyz';
+	var includeUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var includeNumbers = '0123456789';
+	var includeSymbols = '@%+\/!#$?';
+	
+	var includeElements = '';
+	
+	if (document.getElementById('includeLowercase').checked === true) {		
+		var includeElements = includeElements + includeLowercase;		
+	}
+	
+	if (document.getElementById('includeUppercase').checked === true) {		
+		var includeElements = includeElements + includeUppercase;		
+	}
+	
+	if (document.getElementById('includeNumbers').checked === true) {		
+		var includeElements = includeElements + includeNumbers;		
+	}
+	
+	if (document.getElementById('includeSymbols').checked === true) {		
+		var includeElements = includeElements + includeSymbols;		
+	}
+	
+	if (document.getElementById('includeLowercase').checked === false &&
+		document.getElementById('includeUppercase').checked === false && 
+		document.getElementById('includeNumbers').checked === false && 
+		document.getElementById('includeSymbols').checked === false) {
+		
+		console.log('Error code goes here');
+		
+	}
+
+	for (i = 1; i <= 8; i++) { 
+		
+		var char = Math.floor(Math.random() * includeElements.length + 1); 
+		password += includeElements.charAt(char) 
+	} 
+	
+	return password; 
+} 
+function readySetGo() { 
+	document.getElementById('randomResult').value = generatePassword();
+} 
